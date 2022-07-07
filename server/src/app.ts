e8c450c1cae51622cd2274e545ae8fb83d32c7e1
Tomasz Dom <tomaszdom@MacBook-Air-Tomasz.local>
2022-07-07 09:35:44 +0200

    amendments to erstwhile code

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,18 +1,9 @@
 import express, {Request, Response} from 'express';
 import 'dotenv/config';
 
-const mysqlConfig = {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'shop',
-}
-
-let con = null;
-
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+console.log(process.env.NODE_ENV)
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`)
