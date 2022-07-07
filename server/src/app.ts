eaa25162ffb1028402d21e6a6ea1d14b5bc2f5f8
noritorihotondo <63160903+noritorihotondo@users.noreply.github.com>
2022-07-07 21:27:26 +0200

    Adding and dockerizing database (#3)
    * Adding and dockerizing database
    
    * amendments to erstwhile code
    
    * deleting double files, correcting environment
    
    * files clean-up
    
    Co-authored-by: Tomasz Dom <tomaszdom@MacBook-Air-Tomasz.local>

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
-import express from 'express';
+import express, {Request, Response} from 'express';
 import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`)
 })
+
