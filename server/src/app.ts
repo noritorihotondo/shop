6dc315c86f2a219f2c0e4864bb55094020f12942
Tomasz Dom <tomaszdom@MacBook-Air-Tomasz.local>
2022-07-07 14:58:37 +0200

    deleting double files, correcting environment

--- server/src/app.ts
+++ server/src/app.ts
@@ -3,7 +3,6 @@ import 'dotenv/config';
 
 const app = express();
 const PORT = process.env.PORT || 5000;
-console.log(process.env.NODE_ENV)
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port ${PORT}`)
